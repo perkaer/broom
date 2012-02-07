0218fd59b3ac6781bb10aa28dc9aaaef01bf4e91
Per Kaer <evilspaam@gmail.com>
2012-02-07 20:40:04 +0100

    added Sweeper class

--- itertools_fun.py
+++ itertools_fun.py
@@ -23,39 +23,63 @@ def update_dict_if_key_exists(updatee, updater):
     updatee.update({k: v for k, v in updater.iteritems() if k in updatee.keys()})
 
 
-# use odict to control the loop order of the entries
-input_dict = odict()
-input_dict['g'] = np.arange(8, 10)
-input_dict['s'] = np.arange(11, 15)
-input_dict['bs'] = np.arange(3, 5)
+def how_does_dict_pass(*args):
+    for a in args:
+        print a
 
-print input_dict
 
-dict1 = {'s': 1,
-         'as': 'sad'}
+class Sweeper(object):
+    """
+    class for doing parameter investigations, by providing a convenient way
+    of looping over nested loops
+    """
 
-dict2 = {'g': 1,
-         'as': 'sad'}
+    import itertools as it
+    # import numpy as np
+    # from misctools import datestamp
+    from collections import OrderedDict as odict
 
-print dict1
-print dict2
+    def __init__(self, *args):
+        self.sweep_dict = odict()
+        for a in args:
+            self.sweep_dict.update(a)
 
-# update_dict_if_key_exists(dict1, input_dict)
-# update_dict_if_key_exists(dict2, input_dict)
 
-# print dict1
-# print dict2
+if __name__ == '__main__':
 
-testdict = {}
+    # use odict to control the loop order of the entries
+    input_dict = odict()
+    input_dict['g'] = np.arange(8, 10)
+    input_dict['s'] = np.arange(11, 15)
+    input_dict['bs'] = np.arange(3, 5)
 
-results = []
-for p in it.product(*input_dict.values()):
-    kws = dict(zip(input_dict.keys(), p))
-    print kws
-    update_dict_if_key_exists(dict1, kws)
-    update_dict_if_key_exists(dict2, kws)
+    print input_dict
+
+    dict1 = {'s': 1,
+             'as': 'sad'}
+
+    dict2 = {'g': 1,
+             'as': 'sad'}
+
     print dict1
     print dict2
-    print '*' * 5
-    # laser(dict1, dict2)
-    # results.append(np.prod(p))
+
+    # update_dict_if_key_exists(dict1, input_dict)
+    # update_dict_if_key_exists(dict2, input_dict)
+
+    # print dict1
+    # print dict2
+
+    testdict = {}
+
+    results = []
+    for p in it.product(*input_dict.values()):
+        kws = dict(zip(input_dict.keys(), p))
+        print kws
+        update_dict_if_key_exists(dict1, kws)
+        update_dict_if_key_exists(dict2, kws)
+        print dict1
+        print dict2
+        print '*' * 5
+        # laser(dict1, dict2)
+        # results.append(np.prod(p))
