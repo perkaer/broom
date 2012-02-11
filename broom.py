9c2e6a8b5634cb611922e08681b4205b1fc80ce6
Per Kaer <evilspaam@gmail.com>
2012-02-11 10:19:27 +0100

    small clean up

--- broom.py
+++ broom.py
@@ -73,36 +73,3 @@ class Sweeper(object):
         for n_loop, p in enumerate(it.product(*self.sweep_dict.values())):
             self._n_loop = n_loop
             yield dict(zip(self.sweep_dict.keys(), p))
-
-
-if __name__ == '__main__':
-
-    # define the dict keys and values to loop over like this. the order they
-    # are added determines at which level they are in the nested for loop
-    sw = Sweeper({'a': [1, 2]},
-                 {'b': range(66, 69)},
-                 {'stringjoe': ['l33t', 'haxx0r']})
-
-    # dicts containing default parameters
-    default_params1 = {'a': 5,
-                       'c': 66,
-                       'stringjoe': 'a string!'}
-
-    default_params2 = {'b': 2,
-                       's': 22,
-                       'stringjoe': 'another string!'}
-
-    print 'default_params1', default_params1
-    print 'default_params2', default_params2
-
-    # the self.looper attribute is to be looped over
-    for params in sw.looper:
-        # update default parameter dicts
-        update_dict_if_key_exists(default_params1, params)
-        update_dict_if_key_exists(default_params2, params)
-
-        print 'updated default_params1', default_params1
-        print 'updated default_params2', default_params2
-        print '*' * 5
-
-        # pass dicts to whatever and save results
